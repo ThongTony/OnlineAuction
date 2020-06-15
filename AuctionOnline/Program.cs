e3f4c952ca7b31592c7cf2bd09f4245aff0632e5
khoatruongqa <khoatruongqa@HSSSC1LAP02923>
2020-06-15 23:46:01 +0700

    apply Hosted Service & combine with SignalR

--- AuctionOnline/Program.cs
+++ AuctionOnline/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AuctionOnline.Services;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AuctionOnline
 {
@@ -18,6 +14,10 @@ namespace AuctionOnline
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<TimedHostedService>();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
---
+++ AuctionOnline/Services/TimedHostedService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AuctionOnline.Data;
+using AuctionOnline.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AuctionOnline.Services
+{
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer _timer;
+
+        private readonly IServiceScopeFactory scopeFactory;
+        private readonly IHubContext<SignalRServer> hubContext;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger, IHubContext<SignalRServer> hubContext, IServiceScopeFactory scopeFactory)
+        {
+            _logger = logger;
+            this.hubContext = hubContext;
+            this.scopeFactory = scopeFactory;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+
+                var model = new NotificationProduct
+                {
+                    Name = "Product Hosted Service " + DateTime.Now,
+                    IsAvailable = true
+                };
+
+                dbContext.NotificationProducts.Add(model);
+                dbContext.SaveChanges();
+                hubContext.Clients.All.SendAsync("refreshProducts");
+
+                var count = Interlocked.Increment(ref executionCount);
+
+                _logger.LogInformation(
+                    "Timed Hosted Service is working. Count: {Count}", count);
+            }
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
