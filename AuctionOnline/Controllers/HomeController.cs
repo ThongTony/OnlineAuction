d4162e6d1e47d419235f289bbc152ad4919b66ce
Thong <thong0605@yahoo.com>
2020-06-22 00:22:17 +0700

    merge 22-06

--- AuctionOnline/Controllers/HomeController.cs
+++ AuctionOnline/Controllers/HomeController.cs
@@ -20,24 +20,6 @@ namespace AuctionOnline.Controllers
         {
             ViewBag.IsHome = true;
 
-            //var categories = db.Categories.Where(e => e.ParentId == null).ToList();
-            //foreach (var cate in categories)
-            //{
-            //    var categoryItems = db.CategoryItems.Where(x => x.CategoryId == cate.Id).ToList();
-
-            //    foreach (var categoryItem in categoryItems)
-            //    {
-            //        categoryItem.Item = db.Items.FirstOrDefault(x => x.Id == categoryItem.ItemId);
-            //    }
-            //    cate.CategoryItems = categoryItems;
-            //}
-            //var layoutVM = new LayoutViewModel()
-            //{
-            //    CategoriesVM = CategoryUtility.MapModelsToVMs(categories)
-            //};
-
-            //return View(layoutVM);
-
             List<Category> category = new List<Category>();
             List<Category> categories = db.Categories.ToList();
             category = categories
